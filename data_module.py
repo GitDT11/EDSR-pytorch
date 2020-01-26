ef01dfc7a9abd313e657e51e91727aa9da42884d
Divyang Teotia <divyangteotia@gmail.com>
2020-01-26 16:39:32 -0500

    compact

--- data_module.py
+++ data_module.py
@@ -1,10 +1,9 @@
 import torch
-import torchvision
 import os
 import random
-import numpy as np
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
+import functional as F
 
 
 class DIV2K_x2(Dataset):
@@ -51,7 +50,7 @@ class DIV2K_x2(Dataset):
 
         if self.transform:
             img, label = self.transform(img, label)
-            #label = self.transform(label)
+            # label = self.transform(label)
 
         return img, label
 
@@ -102,104 +101,6 @@ class RandomVerticalFlip(object):
         return img, label
 
 
-try:
-    import accimage
-except:
-    accimage = None
-
-
-def _is_pil_image(img):
-    if accimage is not None:
-        return isinstance(img, (Image.Image, accimage.Image))
-    else:
-        return isinstance(img, Image.Image)
-
-
-def _is_tensor_image(img):
-    return torch.is_tensor(img) and img.ndimension() == 3
-
-
-def _is_numpy_image(img):
-    return isinstance(img, np.ndarray) and (img.ndim in {2, 3})
-
-
-def to_tensor(pic):
-    """Convert a ``PIL Image`` or ``numpy.ndarray`` to tensor.
-
-    See ``ToTensor`` for more details.
-
-    Args:
-        pic (PIL Image or numpy.ndarray): Image to be converted to tensor.
-
-    Returns:
-        Tensor: Converted image.
-    """
-    if not(_is_pil_image(pic) or _is_numpy_image(pic)):
-        raise TypeError('pic should be PIL Image or ndarray. Got {}'.format(type(pic)))
-
-    if isinstance(pic, np.ndarray):
-        # handle numpy array
-        img = torch.from_numpy(pic.transpose((2, 0, 1)))
-        # backward compatibility
-        if isinstance(img, torch.ByteTensor):
-            return img.float().div(255)
-        else:
-            return img
-
-    if accimage is not None and isinstance(pic, accimage.Image):
-        nppic = np.zeros([pic.channels, pic.height, pic.width], dtype=np.float32)
-        pic.copyto(nppic)
-        return torch.from_numpy(nppic)
-
-    # handle PIL Image
-    if pic.mode == 'I':
-        img = torch.from_numpy(np.array(pic, np.int32, copy=False))
-    elif pic.mode == 'I;16':
-        img = torch.from_numpy(np.array(pic, np.int16, copy=False))
-    elif pic.mode == 'F':
-        img = torch.from_numpy(np.array(pic, np.float32, copy=False))
-    elif pic.mode == '1':
-        img = 255 * torch.from_numpy(np.array(pic, np.uint8, copy=False))
-    else:
-        img = torch.ByteTensor(torch.ByteStorage.from_buffer(pic.tobytes()))
-    # PIL image mode: L, P, I, F, RGB, YCbCr, RGBA, CMYK
-    if pic.mode == 'YCbCr':
-        nchannel = 3
-    elif pic.mode == 'I;16':
-        nchannel = 1
-    else:
-        nchannel = len(pic.mode)
-    img = img.view(pic.size[1], pic.size[0], nchannel)
-    # put it from HWC to CHW format
-    # yikes, this transpose takes 80% of the loading time/CPU
-    img = img.transpose(0, 1).transpose(0, 2).contiguous()
-    if isinstance(img, torch.ByteTensor):
-        return img.float().div(255)
-    else:
-        return img
-
-
-def normalize(tensor, mean, std):
-    """Normalize a tensor image with mean and standard deviation.
-
-    See ``Normalize`` for more details.
-
-    Args:
-        tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
-        mean (sequence): Sequence of means for each channel.
-        std (sequence): Sequence of standard deviations for each channely.
-
-    Returns:
-        Tensor: Normalized Tensor image.
-    """
-    if not _is_tensor_image(tensor):
-        raise TypeError('tensor is not a torch image.')
-    # TODO: make efficient
-    for t, m, s in zip(tensor, mean, std):
-        t.sub_(m).div_(s)
-    return tensor
-
-
 class Normalize(object):
     """Normalize a tensor image with mean and standard deviation.
     Given mean: ``(M1,...,Mn)`` and std: ``(S1,..,Sn)`` for ``n`` channels, this transform
@@ -223,7 +124,7 @@ class Normalize(object):
         Returns:
             Tensor: Normalized Tensor image.
         """
-        return normalize(img, self.mean, self.std), normalize(label, self.mean, self.std)
+        return F.normalize(img, self.mean, self.std), normalize(label, self.mean, self.std)
 
     def __repr__(self):
         return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
@@ -244,7 +145,7 @@ class ToTensor(object):
         Returns:
             Tensor: Converted image.
         """
-        return to_tensor(img), to_tensor(label)
+        return F.to_tensor(img), F.to_tensor(label)
 
     def __repr__(self):
         return self.__class__.__name__ + '()'
@@ -269,7 +170,7 @@ class Compose(object):
     def __call__(self, img, label):
         for t in self.transforms:
             img, label = t(img, label)
-            #label = t(label)
+            # label = t(label)
         return img, label
 
     def __repr__(self):
